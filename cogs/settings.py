3145948907368ccc03394bae077d6c998d329b08
ce_phox <paul-stier@gmx.de>
2020-07-16 11:01:42 +0200

    Added Translation to settings.py

--- cogs/settings.py
+++ cogs/settings.py
@@ -34,15 +34,14 @@ class SettingsCog(Cog, name="Settings"):
         settings = get_language_config_by_id(ctx.guild.id)
 
         if args:
-            print(1)
+
             prefixes = config.prefix
             prefixes[str(ctx.guild.id)] = args[0]
             config.save("prefix", prefixes)
-            print(2)
 
             await ctx.send(
                 embed=Embed(description=settings.f_prefix_successfully_changed(await get_prefix(ctx.message)), color=0x34bdeb))
-            print(3)
+
             return
         await ctx.send(embed=Embed(title=settings.f_prefix_settings_embed_title(ctx.guild.name),
                                    description=settings.f_prefix_settings_embed_description(await get_prefix(ctx.message), await get_prefix(ctx.message)),
