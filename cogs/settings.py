d7f3d5dc6e665ddddaba38ac4306c7afe5c5ca22
ce_phox <paul-stier@gmx.de>
2020-07-16 11:00:46 +0200

    Added Translation to settings.py

--- cogs/settings.py
+++ cogs/settings.py
@@ -1,6 +1,6 @@
 from discord import Embed
 from discord.ext.commands import Cog, Bot, group, Context, guild_only, has_permissions
-from utils import get_prefix
+from utils import get_prefix, get_language_config_by_id
 from config import config
 
 
@@ -16,12 +16,14 @@ class SettingsCog(Cog, name="Settings"):
         Manage the Settings of BibleBot
         """
         if ctx.invoked_subcommand is None:
-            embed = Embed(title="Bible Bot Settings for " + ctx.guild.name,
-                          description=f"Please use `{await get_prefix(ctx.message)}settings <setting>` for more Information",
+            settings = get_language_config_by_id(ctx.guild.id)
+            embed = Embed(title=settings.f_general_settings_embed_title(ctx.guild.name),
+                          description=settings.f_general_settings_embed_description(await get_prefix(ctx.message)),
                           color=0x34bdeb)
-            embed.add_field(name="Prefix Settings",
-                            value=f"Current Prefix: `{await get_prefix(ctx.message)}`\n"
-                                  f"Type `{await get_prefix(ctx.message)}settings prefix <new Prefix>` to change the Prefix")
+
+            embed.add_field(name=settings.general_settings_embed_field_prefix_title,
+                            value=settings.f_general_settings_embed_field_prefix_description(
+                                await get_prefix(ctx.message), await get_prefix(ctx.message)))
 
             await ctx.send(embed=embed)
 
@@ -29,13 +31,19 @@ class SettingsCog(Cog, name="Settings"):
     @has_permissions(administrator=True)
     @guild_only()
     async def prefix(self, ctx: Context, *args):
+        settings = get_language_config_by_id(ctx.guild.id)
+
         if args:
+            print(1)
             prefixes = config.prefix
             prefixes[str(ctx.guild.id)] = args[0]
             config.save("prefix", prefixes)
+            print(2)
 
-            await ctx.send(embed=Embed(description=f"Prefix changed to `{await get_prefix(ctx.message)}`", color=0x34bdeb))
+            await ctx.send(
+                embed=Embed(description=settings.f_prefix_successfully_changed(await get_prefix(ctx.message)), color=0x34bdeb))
+            print(3)
             return
-        await ctx.send(embed=Embed(title="Prefix for " + ctx.guild.name,
-                                   description=f"Your current Prefix: `{await get_prefix(ctx.message)}`\n"
-                                               f"Change by typing `{await get_prefix(ctx.message)}settings prefix <new Prefix>`", color=0x34bdeb))
+        await ctx.send(embed=Embed(title=settings.f_prefix_settings_embed_title(ctx.guild.name),
+                                   description=settings.f_prefix_settings_embed_description(await get_prefix(ctx.message), await get_prefix(ctx.message)),
+                                   color=0x34bdeb))
